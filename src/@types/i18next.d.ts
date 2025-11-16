8bf95353a48100661f13f778ae6cf0fd07a4ec09
Sergei Terekhov <isergeiterehov@gmail.com>
2025-11-16 14:16:09 +0300

    Исправлена типизация для i18n

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -4,7 +4,7 @@ import en from "@/locales/en.json";
 declare module "i18next" {
   interface CustomTypeOptions {
     resources: {
-      en: typeof en;
+      translation: typeof en.translation;
     };
   }
 }
